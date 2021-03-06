7a481d0f31d1cc55d421f7e59656adbad5e125a6
e-dschungel <github@e-dschungel.de>
2021-03-06 11:27:28 +0100

    added handler test config

--- config/config.dist.php
+++ config/config.dist.php
@@ -12,6 +12,15 @@ $pvc_config = array(
       "php73" => "7.3",
       "php74" => "7.4",
       "php80" => "8.0",
-    )
+    ),
+    "handler_tests" => array(
+      "php56-cgi" => "5.6",
+      "php70-cgi" => "7.0",
+      "php71-cgi" => "7.1",
+      "php72-cgi" => "7.2",
+      "php73-cgi" => "7.3",
+      "php74-cgi" => "7.4",
+      "php80-cgi" => "8.0",
+    ),
 
 );
