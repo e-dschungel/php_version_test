d9410fb44c34efb24946fcbe8d8c791799e51239
Oliver Scharf <oliver@e-dschungel.de>
2021-03-06 11:10:57 +0100

    fixed wrong variable names

--- src/handler_test.php
+++ src/handler_test.php
@@ -2,7 +2,7 @@
 
 function create_handler_test_file($pvc_config, $handler){
     $htaccess_test_code = "AddHandler " . $handler . " .php";
-    create_test_file($pvc_config, "handlertest.php", $htaccess_test_code);
+    create_test_file($pvc_config, ".htaccess", $htaccess_test_code);
     create_version_test_file($pvc_config, "handlertest.php");
 }
 
@@ -10,7 +10,7 @@ function create_handler_test_file($pvc_config, $handler){
 function perform_handler_tests($pvc_config){
     foreach ($pvc_config["handler_tests"] as $handler => $expected_version){
         $testfilename = "handlertest.php";
-        create_handler_test_file($pvc_config, $extension);
+        create_handler_test_file($pvc_config, $handler);
         $actual_version = get_actual_version($pvc_config, $testfilename);
         if ($actual_version != $expected_version){
             echo "Handler " . $handler . " executes version " . $actual_version . " not the expenced version " . $expected_version . "!\n";
