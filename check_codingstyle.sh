a471af18bc66de060a5b37fb372755e2db2dc70a
e-dschungel <github@e-dschungel.de>
2021-03-07 07:35:42 +0100

    added infrastructure for checking codingstyle

---
+++ check_codingstyle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PHPCS_EXEC="./vendor/bin/phpcs"
+PHPCS_IGNORE_PATHS="*/vendor/*,*.phpdoc/*"
+PHPCOMPATIBILITY_IGNORE_PATHS="*/vendor/squizlabs/*,*/vendor/PHPCompatibility/*"
+MIN_PHP_VERSION="5.6"
+
+echo "Check PHP Coding Standard PSR12"
+$PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=psr12 .
+
+echo "Check PHP Coding Standard PEAR (modified)"
+$PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS -s --standard=mypear.xml .
+
+echo "Check PHP Compatibility > $MIN_PHP_VERSION"
+$PHPCS_EXEC --ignore=$PHPCS_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
+#$PHPCS_EXEC --ignore=$PHPCOMPATIBILITY_IGNORE_PATHS --standard=PHPCompatibility --runtime-set testVersion ${MIN_PHP_VERSION}- .
